1021e3ea12b739d3495205caa41558b4ff19b0e4
dgtie <42087847+dgtie@users.noreply.github.com>
2019-06-25 14:28:53 +0800

    Delete rx.py

--- sources/rx.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import serial, struct
-
-ser = serial.Serial('/dev/serial0', 115200)
-while True:
-  val = struct.unpack('<BBH', ser.read(4))
-  print "{:08b}".format(val[2]), val
